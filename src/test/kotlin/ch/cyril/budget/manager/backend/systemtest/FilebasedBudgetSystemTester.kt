6cb91dbe7ec42969f1c5f3aff5d4f644b5f1ab1a
Cyril Steimer <cyril.steimer@gmail.com>
2018-12-19 15:04:17 +0100

    Extract useful code into HttpClient utility.

--- src/test/kotlin/ch/cyril/budget/manager/backend/systemtest/FilebasedBudgetSystemTester.kt
+++ src/test/kotlin/ch/cyril/budget/manager/backend/systemtest/FilebasedBudgetSystemTester.kt
@@ -3,15 +3,7 @@ package ch.cyril.budget.manager.backend.systemtest
 import ch.cyril.budget.manager.backend.main.ServerType
 import ch.cyril.budget.manager.backend.main.main
 import ch.cyril.budget.manager.backend.model.*
-import ch.cyril.budget.manager.backend.rest.GSON
 import ch.cyril.budget.manager.backend.util.SubList
-import com.google.gson.reflect.TypeToken
-import io.ktor.client.HttpClient
-import io.ktor.client.request.delete
-import io.ktor.client.request.get
-import io.ktor.client.request.post
-import io.ktor.client.request.put
-import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions
 import java.math.BigDecimal
 import java.nio.file.Files
@@ -19,8 +11,10 @@ import java.nio.file.Path
 
 class FilebasedBudgetSystemTester(
         private val tempDir: Path,
-        private val server: ServerType,
-        private val port: Int) : AutoCloseable {
+        server: ServerType,
+        port: Int) : AutoCloseable {
+
+    private val client: HttpClient
 
     private val budgetContent = """
         Budget1,900,monthly
@@ -71,8 +65,8 @@ class FilebasedBudgetSystemTester(
                             MonthYear(12, 2020))))
 
     init {
+        client = HttpClient(port)
         budgetFile = Files.write(tempDir.resolve("budgets"), budgetContent.toByteArray())
-
         val expensesFile = Files.createFile(tempDir.resolve("expenses"))
         val config = ParamBuilder.fileBased(expensesFile, budgetFile, ServerType.KTOR, port)
         val configFile = Files.write(tempDir.resolve("config.json"), config.toByteArray())
@@ -97,96 +91,60 @@ class FilebasedBudgetSystemTester(
     }
 
     private fun getAllBudgets () {
-        val budgets = getJson<SubList<Budget>>("/api/v1/budget")
+        val budgets = client.getJson<SubList<Budget>>("/api/v1/budget")
         Assertions.assertEquals(2, budgets.count)
         Assertions.assertEquals(budget1, budgets.values[0])
         Assertions.assertEquals(budget2, budgets.values[1])
     }
 
     private fun getBudgetByCategory () {
-        var budget: Budget = getJson("/api/v1/budget/category/Budget1")
+        var budget: Budget = client.getJson("/api/v1/budget/category/Budget1")
         Assertions.assertEquals(budget1, budget)
-        budget = getJson("/api/v1/budget/category/Budget2")
+        budget = client.getJson("/api/v1/budget/category/Budget2")
         Assertions.assertEquals(budget2, budget)
     }
 
     private fun getCategories () {
-        val categories = getJson<SubList<Category>>("/api/v1/category")
+        val categories = client.getJson<SubList<Category>>("/api/v1/category")
         Assertions.assertEquals(2, categories.count)
         Assertions.assertEquals(budget1.category, categories.values[0])
         Assertions.assertEquals(budget2.category, categories.values[1])
     }
 
     private fun updateBudget () {
-        put("/api/v1/budget", newBudget1)
-        val budget = getJson<Budget>("/api/v1/budget/category/Budget1")
+        client.put("/api/v1/budget", newBudget1)
+        val budget = client.getJson<Budget>("/api/v1/budget/category/Budget1")
         Assertions.assertEquals(newBudget1, budget)
         //TODO Check that the fîle was written
     }
 
     private fun addBudget () {
-        post("/api/v1/budget", newBudget3)
-        val budget = getJson<Budget>("/api/v1/budget/category/Budget3")
+        client.post("/api/v1/budget", newBudget3)
+        val budget = client.getJson<Budget>("/api/v1/budget/category/Budget3")
         Assertions.assertEquals(newBudget3, budget)
         //TODO Check that the file was written
     }
 
     private fun deleteBudget () {
-        delete("/api/v1/budget?category=Budget3")
+        client.delete("/api/v1/budget?category=Budget3")
         //TODO Can we determine the exception more exactly? Or even get the status code?
-        Assertions.assertThrows(Exception::class.java) { getJson<Budget>("/api/v1/budget/category/Budget3") }
+        Assertions.assertThrows(Exception::class.java) { client.getJson<Budget>("/api/v1/budget/category/Budget3") }
         //TODO Check that the file was written
     }
 
     private fun addExistingBudget () {
-        Assertions.assertThrows(Exception::class.java) { post("/api/v1/budget", budget1) }
-        val budget = getJson<Budget>("/api/v1/budget/category/Budget1")
+        Assertions.assertThrows(Exception::class.java) { client.post("/api/v1/budget", budget1) }
+        val budget = client.getJson<Budget>("/api/v1/budget/category/Budget1")
         Assertions.assertEquals(newBudget1, budget)
     }
 
     private fun updateNotExistingBudget () {
         val budget4 = Budget(Category("Budget4"), budget1.amounts)
-        Assertions.assertThrows(Exception::class.java) { put("/api/v1/budget", budget4) }
-        Assertions.assertThrows(Exception::class.java) { getJson<Budget>("/api/v1/budget/category/Budget4") }
+        Assertions.assertThrows(Exception::class.java) { client.put("/api/v1/budget", budget4) }
+        Assertions.assertThrows(Exception::class.java) { client.getJson<Budget>("/api/v1/budget/category/Budget4") }
     }
 
     private fun deleteNotExistingBudget () {
-        Assertions.assertThrows(Exception::class.java) { delete("/api/v1/budget?category=Budget4") }
+        Assertions.assertThrows(Exception::class.java) { client.delete("/api/v1/budget?category=Budget4") }
     }
-
-    private fun put (path: String, body: Any) {
-        val client = HttpClient()
-        runBlocking {
-            client.put<Unit>(getUrl(path)) {
-                this.body = GSON.toJson(body)
-            }
-        }
-    }
-
-    private fun post (path: String, body: Any) {
-        val client = HttpClient()
-        runBlocking {
-            client.post<Unit>(getUrl(path)) {
-                this.body = GSON.toJson(body)
-            }
-        }
-    }
-
-    private fun delete (path: String) {
-        val client = HttpClient()
-        runBlocking {
-            client.delete<Unit>(getUrl(path))
-        }
-    }
-
-    private inline fun <reified T> getJson (path: String): T {
-        val client = HttpClient()
-        val type = object: TypeToken<T>() {}.type
-        return runBlocking {
-            val json = client.get<String>(getUrl(path))
-            GSON.fromJson<T>(json, type)
-        }
-    }
-
-    private fun getUrl (path: String) = "http://127.0.0.1:$port$path"
 }
---
+++ src/test/kotlin/ch/cyril/budget/manager/backend/systemtest/HttpClient.kt
@@ -0,0 +1,49 @@
+package ch.cyril.budget.manager.backend.systemtest
+
+import ch.cyril.budget.manager.backend.rest.GSON
+import com.google.gson.reflect.TypeToken
+import io.ktor.client.HttpClient
+import io.ktor.client.request.delete
+import io.ktor.client.request.get
+import io.ktor.client.request.post
+import io.ktor.client.request.put
+import kotlinx.coroutines.runBlocking
+
+class HttpClient(private val port: Int) {
+
+    fun put (path: String, body: Any) {
+        val client = HttpClient()
+        runBlocking {
+            client.put<Unit>(getUrl(path)) {
+                this.body = GSON.toJson(body)
+            }
+        }
+    }
+
+    fun post (path: String, body: Any) {
+        val client = HttpClient()
+        runBlocking {
+            client.post<Unit>(getUrl(path)) {
+                this.body = GSON.toJson(body)
+            }
+        }
+    }
+
+    fun delete (path: String) {
+        val client = HttpClient()
+        runBlocking {
+            client.delete<Unit>(getUrl(path))
+        }
+    }
+
+    inline fun <reified T> getJson (path: String): T {
+        val client = HttpClient()
+        val type = object: TypeToken<T>() {}.type
+        return runBlocking {
+            val json = client.get<String>(getUrl(path))
+            GSON.fromJson<T>(json, type)
+        }
+    }
+
+    fun getUrl (path: String) = "http://127.0.0.1:$port$path"
+}
