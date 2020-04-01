2a906be3637eab7b1bd3813ac000f6581059d2ec
Cyril Steimer <cyril.steimer@ergon.ch>
2020-04-01 07:44:51 +0200

    Versioned import/export of data.
    When exporting data, the version of that export's format is now indicated in the JSON. This allows us to make changes to the model in the future and still handle the import from old versions.

--- src/main/kotlin/ch/cyril/budget/manager/backend/rest/ImportExportRestHandler.kt
+++ src/main/kotlin/ch/cyril/budget/manager/backend/rest/ImportExportRestHandler.kt
@@ -9,8 +9,16 @@ import ch.cyril.budget.manager.backend.service.budget.BudgetDao
 import ch.cyril.budget.manager.backend.service.expense.ActualExpenseDao
 import ch.cyril.budget.manager.backend.service.expense.ExpenseTemplateDao
 import ch.cyril.budget.manager.backend.service.expense.ScheduledExpenseDao
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
 
-class ImportExport(
+private const val V1 = "V1";
+
+private class VersionedImportExport(
+        val version: String,
+        val content: JsonElement)
+
+private class ImportExportV1(
         val budgets: List<Budget>,
         val expenses: List<ActualExpense>,
         val templates: List<ExpenseTemplate>?,
@@ -24,16 +32,31 @@ class ImportExportRestHandler(
 
     @HttpMethod(HttpVerb.GET, "/api/v1/export")
     fun export(): RestResult {
-        val res = ImportExport(
+        val values = ImportExportV1(
                 budgetDao.getBudgets().values,
                 expenseDao.getExpenses().values,
                 templateDao.getExpenses().values,
                 scheduledExpenseDao.getExpenses().values)
+        val res = VersionedImportExport(V1, GSON.toJsonTree(values))
         return RestResult.json(GSON.toJson(res))
     }
 
     @HttpMethod(HttpVerb.POST, "/api/v1/import")
-    fun import(@Body values: ImportExport) {
+    fun import(@Body values: JsonObject) {
+        // Previous versions made an export without a version indication
+        if (values.has("version")) {
+            val versioned = GSON.fromJson(values, VersionedImportExport::class.java)
+            when (versioned.version) {
+                V1 -> importV1(GSON.fromJson(versioned.content, ImportExportV1::class.java))
+                else -> throw IllegalStateException("Unknown serialization version " + versioned.version)
+            }
+        } else {
+            importV1(GSON.fromJson(values, ImportExportV1::class.java))
+        }
+
+    }
+
+    private fun importV1(values: ImportExportV1) {
         for (budget in values.budgets) {
             if (budgetDao.getOneBudget(budget.category) != null) {
                 budgetDao.updateBudget(budget)
@@ -42,7 +65,7 @@ class ImportExportRestHandler(
             }
         }
         //TODO How can we do an import while updating existing expenses/templates/scheduled expenses?
-        val oldIdToNewExpense: MutableMap<Id, ActualExpense> = HashMap();
+        val oldIdToNewExpense = HashMap<Id, ActualExpense>();
         for (expense in values.expenses) {
             val oldId = expense.id
             val newExpense = expenseDao.addExpense(expense.withoutId())
